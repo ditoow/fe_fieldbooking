18a23d94700becc42bdd8de7422848b4a5787b88
Raffael Ezra <nugrohoraffael@gmail.com>
2026-07-11 00:34:40 +0700

    Add cache buster to logo to force browser refresh

--- src/app/(landingpage)/components/Navbar.tsx
+++ src/app/(landingpage)/components/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
           {/* Logo */}
           <div className="flex-1 flex items-center">
             <Link href="/" className="flex items-center gap-2 hover:opacity-80 transition shrink-0">
-              <img src="/logo.png" alt="Pivactive Logo" width={120} height={40} className="object-contain" />
+              <img src="/logo.png?v=2" alt="Pivactive Logo" width={120} height={40} className="object-contain" />
             </Link>
           </div>
 
